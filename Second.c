deb6e70f5e85f952da2221db88cb7a8150752daa
Noneed-toknow <145554587+Noneed-toknow@users.noreply.github.com>
2023-10-31 11:54:40 +0530

    Add files via upload

---
+++ Second.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a = 6, d;
+    d = a;
+    printf("The value of a + d is = %d\n", d+d);
+    printf("The value of a - d is = %d\n", d-d);
+    printf("The value of a * d is = %d\n", d*d);
+    printf("The value of a / d is = %d\n", d/d);
+    float b = 3.5;
+    char c = 'A';
+    printf("The value of a is: %d", a);
+    printf("\nThe value of d is: %d", d);
+    int ted;
+    // 
+    return 0;
+}
