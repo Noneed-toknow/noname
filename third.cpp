7fc626674068966efc4fb81f43748402b915272c
Noneed-toknow <145554587+Noneed-toknow@users.noreply.github.com>
2023-10-31 18:01:56 +0530

    (A proper calculator)third.cpp
    This is the updated version of third.cpp where we were performing a single operation, which was probably multiplication, here we are asking the user to input the no. as well as the operation they want to perform.

--- third.cpp
+++ third.cpp
@@ -4,6 +4,28 @@ int main()
     int a, b;
     std::cout<<"Enter the values of a and b respectively: " << std::endl;
     std::cin>>a>>b;
-    std::cout<<"The multiplication of the two no. comes out to be: " << a * b << std::endl;
-    return 0;
+    char op;
+    std::cout<<"What kind of operation do you want to perform on the 2 no.?"<<"\n"<<"Enter either '+', '-', '*', or '/'" << std::endl;
+    std::cin>>op;
+    if (op = '-')
+    {
+        std::cout<<"The subtraction of the two no. comes out to be: " << a - b << std::endl;
+    }
+    else if (op = '+')
+    {
+        std::cout<<"The sum of the two no. comes out to be: " << a+b << std::endl;
+    }
+    else if (op = '*')
+    {
+        std::cout<<"The product of the two no. comes out to be: " << a*b << std::endl;
+    }
+    else if (op = '/')
+    {
+        std::cout<<"The sum of the two no. comes out to be: " << a/b << std::endl;
+    }
+    else
+    {
+        std::cout << "Enter a valid operation" << std::endl;
+    }
+        return 0;
 }
