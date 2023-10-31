0a02082f61387d7efffd0c7247e43817e68fc090
Prashant Kumar <kumarprashant5502@gmail.com>
2023-10-31 17:37:02 +0530

    Just adding a cpppppp file

---
+++ third.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main()
+{
+    int a, b;
+    std::cout<<"Enter the values of a and b respectively: " << std::endl;
+    std::cin>>a>>b;
+    std::cout<<"The multiplication of the two no. comes out to be: " << a * b << std::endl;
+    return 0;
+}
